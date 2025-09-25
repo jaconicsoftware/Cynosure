58d71b49e6122d3a75b3c99a3a5325161fae04f7
Jaconic Software <154589977+jaconicsoftware@users.noreply.github.com>
2025-09-25 19:21:40 +0000

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,14 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from models import Base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+# ⚠️ подключение из переменной окружения
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+
+# создаём таблицы при старте
+def init_db():
+    Base.metadata.create_all(bind=engine)
